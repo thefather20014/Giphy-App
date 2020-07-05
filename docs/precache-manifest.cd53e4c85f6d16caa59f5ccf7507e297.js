a7c5f458530787970f8789e58d2961b70d31b0e7
Carlos Aracena <carlosthefather2014@hotmail.com>
2020-07-05 18:57:47 -0400

    Second Commit

---
+++ docs/precache-manifest.cd53e4c85f6d16caa59f5ccf7507e297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248a74a41b7d335ce10c8c6b9f252fe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a386547c7a11da8da451",
+    "url": "/static/css/main.e283ee50.chunk.css"
+  },
+  {
+    "revision": "857f45b47ba9c2d8b478",
+    "url": "/static/js/2.92c78188.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.92c78188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a386547c7a11da8da451",
+    "url": "/static/js/main.3265d6f6.chunk.js"
+  },
+  {
+    "revision": "e13af855f2385358fc05",
+    "url": "/static/js/runtime-main.17c06fbb.js"
+  }
+]);
